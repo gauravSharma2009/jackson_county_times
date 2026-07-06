8dbb211cf127061f5248474fd24f7c778a5232fc
gauravSharma2009 <62610758+gauravSharma2009@users.noreply.github.com>
2026-07-06 22:27:05 +0530

    splash and package correction

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,7 @@
-export const BASE_URL = 'http://54.144.41.237:6200/api';
-export const SERVER_URL = 'http://54.144.41.237:6200';
+// export const BASE_URL = 'http://54.144.41.237:6200/api';
+// export const SERVER_URL = 'http://54.144.41.237:6200';
+
+
+export const BASE_URL = 'https://jct.webportalonline.com/api';
+export const SERVER_URL = 'https://jct.webportalonline.com';
+
